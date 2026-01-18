47b85cd35c88f856e496a6d81eb0bd69f934380e
“bakosh2” <bakosh2@hotmail.com>
2026-01-18 09:50:00 +0000

    add the code to the file

--- python-if_else_loops_functions/4-print_hexa.py
+++ python-if_else_loops_functions/4-print_hexa.py
@@ -1,4 +1,3 @@
 #!/usr/bin/python3
-for i in range(97, 123):
-    if i not in (ord('e'), ord('q')):
-        print("{}".format(chr(i)), end="")
+for i in range(99):
+    print("{} = {}".format(i, hex(i)))
