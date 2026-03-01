e1cbbefcc3a09fc3b25e1ab9a331868bdc2dffac
“bakosh2” <bakosh2@hotmail.com>
2026-03-01 08:47:14 +0000

    fix

--- SQL_introduction/1-create_database_if_missing.sql
+++ SQL_introduction/1-create_database_if_missing.sql
@@ -1,5 +1,5 @@
 -- A script that creates the database hbtn_0c_0 in your MySQL server.
-CREATE DATABASE IF NOT EXISTS hbtn_0c_xaxa;
+CREATE DATABASE IF NOT EXISTS hbtn_0c_0;
 ~                                                                                                                                                                           
 ~                                                                                                                                                                           
 ~                                                                                                                                                                           
